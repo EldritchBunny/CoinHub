a69d4e1486149f1eff6caddb548f434837091d3d
EldritchBunny <rebecca.grn19@gmail.com>
2022-12-06 12:59:23 +0100

    added more coins

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,13 @@
 const inputEmail = document.querySelector("#email");
-const submitBtn = document.querySelector(".button");
+const errorMsg = document.querySelector("#error");
+const submitBtn = document.querySelector("#button");
 
 submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
   if (inputEmail.value == "") {
     errorMsg.innerHTML = "oops you forgot the email";
+  } else {
+    window.location.reload();
   }
 });
